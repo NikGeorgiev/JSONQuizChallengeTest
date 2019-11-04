bc22455c38e2e5205dc4982b9c002e38e1175373
NGeorgiev <NGeorgiev@spartaglobal.com>
2019-11-04 11:50:57 +0000

    initial commit

---
+++ src/test/java/com/spartaglobal/JSONQuizChallenge/AppTest.java
@@ -0,0 +1,20 @@
+package com.spartaglobal.JSONQuizChallenge;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
